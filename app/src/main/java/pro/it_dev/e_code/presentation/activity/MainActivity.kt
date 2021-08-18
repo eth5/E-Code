01e016691e900f2ff3295022bc48af880c121f85
xWeb <29051616+eth5@users.noreply.github.com>
2021-08-18 16:27:27 +1000

    add theme surface

--- app/src/main/java/pro/it_dev/e_code/presentation/activity/MainActivity.kt
+++ app/src/main/java/pro/it_dev/e_code/presentation/activity/MainActivity.kt
@@ -3,6 +3,8 @@ package pro.it_dev.e_code.presentation.activity
 import android.os.Bundle
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.platform.ComposeView
 import dagger.hilt.android.AndroidEntryPoint
 import pro.it_dev.e_code.R
@@ -10,6 +12,7 @@ import pro.it_dev.e_code.ad.createBanner
 import pro.it_dev.e_code.ad.initialBanner
 import pro.it_dev.e_code.data.IData
 import pro.it_dev.e_code.data.utils.AssetMover
+import pro.it_dev.e_code.presentation.activity.ui.theme.ECodeTheme
 import pro.it_dev.e_code.presentation.nav.Navigation
 import javax.inject.Inject
 
@@ -23,7 +26,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<ComposeView>(R.id.main_container).apply {
-            setContent{ Navigation() }
+            setContent{
+                ECodeTheme() {
+                    Surface(color = MaterialTheme.colors.background) {
+                        Navigation() }
+                    }
+                }
+
         }
         assetMoveData()
         initialAd(findViewById<ViewGroup>(R.id.adContent))
