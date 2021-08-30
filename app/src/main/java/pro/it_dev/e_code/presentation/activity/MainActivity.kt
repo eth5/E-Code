43e1194303c17eae509530d3bd14855a0038d85f
xWeb <29051616+eth5@users.noreply.github.com>
2021-08-30 20:22:27 +1000

    README.md

--- app/src/main/java/pro/it_dev/e_code/presentation/activity/MainActivity.kt
+++ app/src/main/java/pro/it_dev/e_code/presentation/activity/MainActivity.kt
@@ -44,7 +44,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initialAd(viewGroup: ViewGroup){
-        return
         val adUtil = AdBannerUtil()
         val adView = adUtil.createBanner(this)
         viewGroup.addView(adView)
