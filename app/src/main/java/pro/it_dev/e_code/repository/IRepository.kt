a02c77ebb1ef95e947188b92c2c9f809db6f03f6
xWeb <29051616+eth5@users.noreply.github.com>
2021-08-30 22:50:05 +1000

    remove todo

--- app/src/main/java/pro/it_dev/e_code/repository/IRepository.kt
+++ app/src/main/java/pro/it_dev/e_code/repository/IRepository.kt
@@ -6,6 +6,6 @@ import pro.it_dev.e_code.utils.Resource
 
 interface IRepository {
     suspend fun getById(id: Int): Resource<ECode>
-    suspend fun getAllMinial(): Resource<List<ECodeMinimal>>//todo переписать на получение только базовой инфо для листа
+    suspend fun getAllMinial(): Resource<List<ECodeMinimal>>
     suspend fun saveAll(list: List<ECode>): Resource<String>
 }
